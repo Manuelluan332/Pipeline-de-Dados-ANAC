83898dc77417ef58d686adad1af7e025849ed04f
Manuel.Luan <manuelluanassis33@gmail.com>
2026-03-25 15:25:50 -0300

    Adição e alterações no repositório

--- Anac/Transact-SQL/Mapeamento de Dados na tabela.sql
+++ Anac/Transact-SQL/Mapeamento de Dados na tabela.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS Anac_Mapeamento(
-	Name_Organizacao varchar(50),
-	Cod_Pad int,
-	Parametro_name varchar(85),
+	 ORG_NOME varchar(75),
+	PAPP_COD int,
+	PAPP_NOME varchar(85),
 	PAPP_PN VARCHAR(85),
 	APAA_CODI varchar(85), 
-	Data_Arpp TIMESTAMP,
-	APAA_STATUS VARCHAR(2)
+	APAA_DATA  TIMESTAMP,
+	APAA_STATUS VARCHAR(10)
+
 );
 
 select * from Anac_Mapeamento
